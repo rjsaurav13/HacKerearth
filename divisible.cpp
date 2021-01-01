21f7663f835b971bc1b3e6111defa2b60372a757
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-01-01 21:34:49 +0530

    Update divisible.cpp

--- divisible.cpp
+++ divisible.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
+#include<string>
+#include <stdio.h>
 using namespace std;
 int main(){
-int tcase,len;
-cin>>tcase;
-
+int temp=0;
+string n;
+cout<<"Enter your number\n";
+cin>>n;
+int count = 0;
+    while (n != 0)
+    {
+        n = n / 10;
+        ++count;
+    }
+cout<<num;
 return 0;
 }
