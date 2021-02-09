1447bc2c53292103a5c6e0c9bc5e22c476043a73
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-02-10 00:41:03 +0530

    c

---
+++ Understand_Computer.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int tcase;
+	cin >> tcase;
+	for(int i=0;i<tcase;i++){
+        long long int num;
+        cin>>num;
+        cout<<num-1<<"\n";
+
+	}
+
+	return 0;
+}
+
