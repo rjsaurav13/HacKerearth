8fd341b223e5ef2bf6d4e62446ee3f903b9eda5b
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-03-10 01:04:50 +0530

    Create Life_the_Universe_and_Everything.cpp

---
+++ Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+   for(;;){
+       int num;
+        cin>>num;
+        if(num==42){
+            break;
+        }
+        cout<<num<<"\n";
+        
+    }
+}
