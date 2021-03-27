099dd95c987ee8652b3274fd52bfd35003eb6821
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-03-28 02:30:42 +0530

    s

---
+++ Spalindromw.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+
+    int tcase;
+    cin>>tcase;
+    while (tcase--)
+    {
+        int n;
+        cin>>tcase;
+        int arr[n];
+        for (int i = 0; i != n - 1; ++i)
+        {
+            cin>>arr[i];
+        }
+        printf("Third\n");
+    }
+}
